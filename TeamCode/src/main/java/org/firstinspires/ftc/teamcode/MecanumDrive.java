059bdb638de832217d5b13c5e9112b343f0ffef0
anton <bezushchenko.anton@gmail.com>
2026-05-11 10:44:07 -0400

    Merge remote-tracking branch 'origin/uthis_main' into uthis_main
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
    
    added negative values on movement for proper control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -46,10 +46,13 @@ public class MecanumDrive extends LinearOpMode {
                 isFieldOriented = !isFieldOriented;
             }
             if (isFieldOriented) {
-                driveFieldOriented(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+                driveFieldOriented(gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);
             }
             else{
-                drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+                drive(gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);
+            }
+            if (gamepad1.yWasPressed()) {
+                imu.resetYaw();
             }
         }
     }
