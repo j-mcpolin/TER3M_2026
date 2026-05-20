2d00e44b3904bfff748a85a5da4e9e391215e762
anton <bezushchenko.anton@gmail.com>
2026-05-20 11:23:32 -0400

    Co-authored-by: Uthizshan <pakeerathanu@gmail.com> Co-authored-by: Radin <Radinsh2009@gmail.com>
    uthi found hue values and added, also fixed small errors in code
    
    Radin did telemetry data
    
    Anton did the color sensor setup code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -1,17 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Color;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import java.util.Locale;
-
-
 
 @TeleOp
 public class MecanumDrive extends LinearOpMode {
@@ -21,16 +20,11 @@ public class MecanumDrive extends LinearOpMode {
     DcMotor backRightDrive;
 
     IMU imu;
-    NormalizedColorSensor colorSensor;
-
-    ColorSensor sensorColor;
-    DistanceSensor sensorDistance;
-
-
     boolean isFieldOriented = true;
 
 
-
+    NormalizedColorSensor colorSensor;
+    final float[] hsvValues = new float[3];
     @Override
     public void runOpMode() {
         frontLeftDrive = hardwareMap.get(DcMotor.class,  "frontLeft");
@@ -41,6 +35,9 @@ public class MecanumDrive extends LinearOpMode {
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
 
+
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "sensor_color");
+
         imu = hardwareMap.get(IMU.class, "imu");
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection =
                 RevHubOrientationOnRobot.LogoFacingDirection.UP;
@@ -50,23 +47,11 @@ public class MecanumDrive extends LinearOpMode {
         RevHubOrientationOnRobot orientationOnRobot = new
                 RevHubOrientationOnRobot(logoDirection, usbDirection);
         imu.initialize(new IMU.Parameters(orientationOnRobot));
-        sensorColor = hardwareMap.get(ColorSensor.class, "sensor_color_distance");
-        sensorDistance = hardwareMap.get(DistanceSensor.class, "sensor_color_distance");
-        float hsvValues[] = {0F, 0F, 0F};
-        final float values[] = hsvValues;
-        final double SCALE_FACTOR = 255;
-        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
-        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
-        waitForStart();
-
-
-
-
-
-
 
         waitForStart();
+        telemetry.setAutoClear(true);
         while(opModeIsActive()) {
+
             if (gamepad1.aWasPressed()){
                 isFieldOriented = !isFieldOriented;
             }
@@ -79,30 +64,14 @@ public class MecanumDrive extends LinearOpMode {
             if (gamepad1.yWasPressed()) {
                 imu.resetYaw();
             }
-            Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
-                    (int) (sensorColor.green() * SCALE_FACTOR),
-                    (int) (sensorColor.blue() * SCALE_FACTOR),
-                    hsvValues);
-            telemetry.addData("Distance (cm)",
-                    String.format(Locale.US, "%.02f", sensorDistance.getDistance(DistanceUnit.CM)));
-            telemetry.addData("Alpha", sensorColor.alpha());
-            telemetry.addData("Red  ", sensorColor.red());
-            telemetry.addData("Green", sensorColor.green());
-            telemetry.addData("Blue ", sensorColor.blue());
-            telemetry.addData("Hue", hsvValues[0]);
-            relativeLayout.post(new Runnable() {
-                public void run() {
-                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
-                }
-            });
+
+
+            updateColorSensor();
+            telemetry.addData("Drive Mode", isFieldOriented ? "Field Oriented" : "Robot Centric");
             telemetry.update();
         }
-        relativeLayout.post(new Runnable() {
-            public void run() {
-                relativeLayout.setBackgroundColor(Color.WHITE);
-            }
-        });
     }
+
     public void drive(double forward, double right, double rotate) {
         double frontLeftPower = forward + right + rotate;
         double frontRightPower = forward - right - rotate;
@@ -116,7 +85,6 @@ public class MecanumDrive extends LinearOpMode {
         maxPower = Math.max(maxPower, Math.abs(frontRightPower));
         maxPower = Math.max(maxPower, Math.abs(backRightPower));
         maxPower = Math.max(maxPower, Math.abs(backLeftPower));
-
 
         frontLeftDrive.setPower(maxSpeed * (frontLeftPower / maxPower));
         frontRightDrive.setPower(maxSpeed * (frontRightPower / maxPower));
@@ -133,5 +101,34 @@ public class MecanumDrive extends LinearOpMode {
         drive(newForward, newRight, rotate);
     }
 
-}
+    private void updateColorSensor() {
+        NormalizedRGBA colors = colorSensor.getNormalizedColors();
 
+        Color.RGBToHSV(
+                (int) (colors.red * 255),
+                (int) (colors.green * 255),
+                (int) (colors.blue * 255),
+                hsvValues
+        );
+
+        float hue = hsvValues[0];
+        float saturation = hsvValues[1];
+        float value = hsvValues[2];
+
+        String detectedColor = "idk bruh";
+
+
+        if (saturation > 0.3) {
+            if (hue >= 145 && hue <= 180) {
+                detectedColor = "Green";
+            } else if (hue >= 225 && hue <= 250) {
+                detectedColor = "Purple";
+            }
+        }
+
+        telemetry.addData("--- Sensor Output ---", "");
+        telemetry.addData("Detected Element", detectedColor);
+        telemetry.addData("Hue Angle (Deg)", "%.1f", hue);
+        telemetry.addData("Saturation/Intensity", "%.2f", saturation);
+    }
+}
